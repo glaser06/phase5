771dd709c884ad98769c6851f94e44caf6efb8b8
zirenz <zirenz@andrew.cmu.edu>
2016-05-13 14:49:33 -0700

    authorization and sessions?

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :check_login
   
   def index
     @active_employees = Employee.active.alphabetical.paginate(page: params[:page]).per_page(10)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,18 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, case_sensitive: false
   validates_format_of :email, :with => /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :message => "is not a valid format"
   validate :employee_is_active_in_system
+
+
+  def self.authenticate(email,password)
+      find_by_email(email).try(:authenticate, password)
+  end
+  ROLES = [['Administrator', :admin],['Manager', :manager],['Employee', :employee]]
+
+  def role?(authorized_role)
+    return false if role.nil?
+    role.to_sym == authorized_role
+  end
+
   
   private
   def employee_is_active_in_system
